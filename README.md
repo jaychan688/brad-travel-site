c9234b6daf5da6cb0d5ebc02aac43c73581a3b88
Jay Chan <jaychan688@gmail.com>
2021-07-13 07:35:46 +0800

    First commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+lodash normalize.css lazysizes
+
+fs-extra webpack webpack-cli webpack-dev-server @babel/core @babel/preset-env @babel/preset-react babel-loader
+style-loader css-loader
+mini-css-extract-plugin clean-webpack-plugin html-webpack-plugin
+cssnano autoprefixer postcss-extend postcss-hexrgba postcss-import postcss-loader postcss-mixins postcss-nested postcss-simple-vars
---
+++ src/js/index.js
@@ -0,0 +1,31 @@
+import '../css/styles.css'
+import 'lazysizes'
+
+import MobileMenu from './modules/MobileMenu.js'
+import RevealOnScroll from './modules/RevealOnScroll'
+import StickyHeader from './modules/StickyHeader'
+
+new StickyHeader()
+new RevealOnScroll(document.querySelectorAll('.feature-item'), 75)
+new RevealOnScroll(document.querySelectorAll('.testimonial'), 70)
+new MobileMenu()
+
+// Code Splitting
+let modal
+document.querySelectorAll('.open-modal').forEach((el) => {
+	el.addEventListener('click', (e) => {
+		e.preventDefault()
+		console.log(modal)
+		if (typeof modal === 'undefined') {
+			// Add prefix name - modal
+			import(/* webpackChunkName: "modal" */ './modules/Modal.js')
+				.then((module) => {
+					modal = new module.default()
+					setTimeout(() => modal.openTheModal(), 20)
+				})
+				.catch(() => console.log('There was a problem.'))
+		} else {
+			modal.openTheModal()
+		}
+	})
+})
