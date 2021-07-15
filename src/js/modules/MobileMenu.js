40b388fff2ef8e818d8c2c285a2e8704b0677d1c
Jay Chan <jaychan688@gmail.com>
2021-07-15 11:30:32 +0800

    Comment until ch14.

--- src/js/modules/MobileMenu.js
+++ src/js/modules/MobileMenu.js
@@ -1,20 +1,24 @@
 class MobileMenu {
-  constructor() {
-    this.menuIcon = document.querySelector('.site-header__menu-icon');
-    this.menuContent = document.querySelector('.site-header__menu-content');
-    this.siteHeader = document.querySelector('.site-header');
-    this.events();
-  }
+	constructor() {
+		// A. Selecting elements from the DOM
+		this.menuIcon = document.querySelector('.site-header__menu-icon')
+		this.menuContent = document.querySelector('.site-header__menu-content')
+		this.siteHeader = document.querySelector('.site-header')
+		// As soon as page load, the event begin listening
+		this.events()
+	}
 
-  events() {
-    this.menuIcon.addEventListener('click', () => this.toggleMenu());
-  }
+	events() {
+		// B. Event handling
+		this.menuIcon.addEventListener('click', () => this.toggleMenu())
+	}
 
-  toggleMenu() {
-    this.menuContent.classList.toggle('site-header__menu-content--is-visible');
-    this.siteHeader.classList.toggle('site-header--is-expended');
-    this.menuIcon.classList.toggle('site-header__menu-icon--close-x');
-  }
+	// C. Defining functionality
+	toggleMenu() {
+		this.menuContent.classList.toggle('site-header__menu-content--is-visible')
+		this.siteHeader.classList.toggle('site-header--is-expended')
+		this.menuIcon.classList.toggle('site-header__menu-icon--close-x')
+	}
 }
 
-export default MobileMenu;
+export default MobileMenu
